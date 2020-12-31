bac6a1852d3495a785c6b6e4ec97f36184d2b529
Christoph Würsch <christoph.wuersch@ost.ch>
2020-12-31 08:43:35 +0100

    Aktualisiere main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,12 +1,14 @@
 input.onButtonPressed(Button.A, function () {
     input.calibrateCompass()
     basic.showLeds(`
-        # . # # #
-        # . . . #
         # . # . #
-        # . . . #
-        # # # # #
+        . . . . .
+        # . # . #
+        . . . . .
+        # . # . #
         `)
+    basic.pause(200)
+    basic.clearScreen()
 })
 let richtung = 0
 basic.showLeds(`
@@ -16,33 +18,36 @@ basic.showLeds(`
     # . . . #
     # # # # #
     `)
+basic.pause(200)
+basic.showString("Hello!")
+basic.clearScreen()
 basic.forever(function () {
     richtung = input.compassHeading()
     if (richtung < 190 && richtung > 170) {
         basic.showArrow(ArrowNames.South)
     } else {
         if (richtung < 235 && richtung > 215) {
-            basic.showArrow(ArrowNames.SouthWest)
+            basic.showArrow(ArrowNames.SouthEast)
         } else {
             if (richtung < 145 && richtung > 125) {
-                basic.showArrow(ArrowNames.SouthEast)
+                basic.showArrow(ArrowNames.SouthWest)
             } else {
                 if (richtung < 100 && richtung > 80) {
-                    basic.showArrow(ArrowNames.East)
+                    basic.showArrow(ArrowNames.West)
                 } else {
                     if (richtung < 280 && richtung > 260) {
-                        basic.showArrow(ArrowNames.West)
+                        basic.showArrow(ArrowNames.East)
                     } else {
                         if (richtung < 10 || richtung > 350) {
                             basic.showArrow(ArrowNames.North)
                         } else {
                             if (richtung < 55 && richtung > 35) {
-                                basic.showArrow(ArrowNames.NorthEast)
+                                basic.showArrow(ArrowNames.NorthWest)
                             } else {
                                 if (richtung < 325 && richtung > 305) {
-                                    basic.showArrow(ArrowNames.NorthWest)
+                                    basic.showArrow(ArrowNames.NorthEast)
                                 } else {
-                                	
+                                    basic.pause(10)
                                 }
                             }
                         }
